d073c99e80fba11a8cc0b43a5c376149de7d370e
etomas <etomas@gmail.com>
2016-12-05 10:18:50 +0100

    Public web site (netcore)

---
+++ PublicWebSite_NetCore/src/BikeSharing.Web.NetCore/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using BikeSharing.Web.Configuration; 
+
+namespace BikeSharing.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IOptions<Links> _links;
+
+        public HomeController (IOptions<Links> links)
+        {
+            _links = links;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.PrivateWebsite = _links.Value.PrivateWebsite;
+            return View();
+        }
+    }
+}
---
+++ PublicWebSite_NetCore/src/BikeSharing.Web.NetCore/Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Options;
+using BikeSharing.Web.Configuration; 
+
+namespace BikeSharing.Web
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Configuration Settings:
+            services.AddOptions();
+            services.Configure<Links>(Configuration.GetSection("Links"));
+
+            services.AddLocalization(options =>
+            {
+                options.ResourcesPath = "Resources";
+            });
+            // Add framework services.
+            services.AddMvc().AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix, options =>
+            {
+                options.ResourcesPath = "Resources";
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+
+            app.UseCors(pb => pb.AllowAnyHeader());
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
